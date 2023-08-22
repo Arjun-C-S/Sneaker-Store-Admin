81a55c9e2d3dd343b0ddba1028e967485f2977a5
Arjun-C-S <arjunchoozhi1234@gmail.com>
2023-08-22 10:58:49 +0530

    setup mariadb database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'mariadb',
+          host: config.get('MARIADB_HOST'),
+          port: +config.get('MARIADB_PORT'),
+          username: config.get('MARIADB_USER'),
+          password: config.get('MARIADB_PASSWORD'),
+          database: config.get('MARIADB_DATABASE'),
+          entities: [],
+          synchronize: config.get('APP_ENV') === 'development',
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+function setupSwagger(app) {
+  const configService: ConfigService = app.get(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Foodiee Application')
+    .setDescription('Online food delivery application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  const swaggerPath = configService.get<string>('SWAGGER_PATH', 'docs');
+  SwaggerModule.setup(swaggerPath, app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService: ConfigService = app.get(ConfigService);
+
+  if (configService.get('APP_ENV') === 'development') {
+    setupSwagger(app);
+  }
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(configService.get<number>('PORT', 8080));
 }
 bootstrap();
