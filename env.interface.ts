fb059e6281849a6608954702482f431ce431c1cb
Arjun-C-S <arjunchoozhi1234@gmail.com>
2023-08-23 12:45:47 +0530

    added interface to env file

---
+++ env.interface.ts
@@ -0,0 +1,8 @@
+export interface DatabaseConfig {
+  MARIADB_PORT: number;
+  MARIADB_HOST: string;
+  MARIADB_USER: string;
+  MARIADB_PASSWORD: string;
+  MARIADB_DATABASE: string;
+  APP_ENV: string;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryModule } from './modules/category/category.module';
 import { CategoryTable } from './modules/category/schema/category.schema';
+import { DatabaseConfig } from 'env.interface';
 
 @Module({
   imports: [
@@ -12,16 +13,16 @@ import { CategoryTable } from './modules/category/schema/category.schema';
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
+      useFactory: (config: ConfigService<DatabaseConfig>) => {
         return {
           type: 'mariadb',
-          host: config.get('MARIADB_HOST'),
-          port: +config.get('MARIADB_PORT'),
-          username: config.get('MARIADB_USER'),
-          password: config.get('MARIADB_PASSWORD'),
-          database: config.get('MARIADB_DATABASE'),
+          host: config.get('MARIADB_HOST', { infer: true }),
+          port: config.get('MARIADB_PORT', { infer: true }),
+          username: config.get('MARIADB_USER', { infer: true }),
+          password: config.get('MARIADB_PASSWORD', { infer: true }),
+          database: config.get('MARIADB_DATABASE', { infer: true }),
           entities: [CategoryTable],
-          synchronize: config.get('APP_ENV') === 'development',
+          synchronize: config.get('APP_ENV', { infer: true }) === 'development',
         };
       },
     }),
